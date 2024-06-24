a05370569b6ee06b1567e1510b37f2e43429f803
sachinpatel1432 <152507178+sachinpatel1432@users.noreply.github.com>
2024-06-24 14:26:27 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,22 @@
+// Calculate the factorial by using the recutssion!!
+public class factorial {
+        public static int fact(int n) {
+                // base case!
+                if (n <= 1) {
+                        return 1;
+                }
+                // condition!
+                int fnm1 = fact(n-1);
+                // calculate factorial!
+                int fn = n * fnm1;
+                // return main!
+                return fn;
+        }
+        public static void main(String[] args) {
+                // Input!
+                int n = 5;
+                System.err.println(fact(n));
+        }
+}
+        // Output = 1*2*3*4*5 = 120:
+        // thank you!!
